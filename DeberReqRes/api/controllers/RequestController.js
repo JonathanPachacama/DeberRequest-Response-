c4762b0a537e98cf3d9137370370b85f2c549da0
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-06-26 19:04:22 -0500

    request

--- DeberReqRes/api/controllers/RequestController.js
+++ DeberReqRes/api/controllers/RequestController.js
@@ -1,3 +1,24 @@
-/**
- * Created by visitante on 26/06/2017.
- */
+module.exports = {
+    optionsValues: function (req, res, next) {
+        //req.options.values
+        //Los valores por defecto para los planos createy updateacciones.
+        sails.log.info(req.method);
+        if (req.session.user) {
+            // Use un existente req.options.values, o inicialicelo a un objeto vacio
+            req.options.values = req.options.values || {};
+            // Setear el default `name` para "crear" y "actualizar" planos
+            req.options.values.name = req.session.user.name;
+        }
+        return next();
+    },
+    optionsValuesWhere: function (req, res, next) {
+        // req.options.values.where
+        if (req.session.user) {
+            // Use un existente req.options.where, o inicialicelo a un objeto vacio
+            req.options.where = req.options.where || {};
+            // Setear el default `userId` para "buscar" y "actualizar" planos
+            req.options.where.userId = req.session.user.id;
+        }
+        return next();
+    }
+};
