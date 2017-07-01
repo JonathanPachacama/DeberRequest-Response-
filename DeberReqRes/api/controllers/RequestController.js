d5a7deafefa52036f88107f35393c27eaa0b5bad
LilianRaquelQuimbitaQuimbita <lilianraquelq@gmail.com>
2017-07-01 00:06:59 -0500

    TerminadoRequest

--- DeberReqRes/api/controllers/RequestController.js
+++ DeberReqRes/api/controllers/RequestController.js
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 /**
  * Created by visitante on 26/06/2017.
  */
 /*Deber */
-/* 1 Ejemplo Accept*/
-accept: function (req, res) {
+
+module.exports = {
+  /* 1 Ejemplo Accept*/
+  metodoaccepted: function (req, res) {
   sails.log.info(req.accepted);
   if (req.accepted) {
     return res.json({saludo: "hola"})
@@ -14,7 +15,7 @@ accept: function (req, res) {
 },
 
 /* 2 Ejemplo AcceptCharset*/
-acceptcharset: function (req, res) {
+metodoacceptcharset: function (req, res) {
   sails.log.info(req.acceptsCharsets);
   req.acceptsCharsets;
   return res.send("Aceptado");
@@ -40,11 +41,12 @@ acceptcharset: function (req, res) {
 
 /* 4 Ejemplo Boddy*/
 metodoReqboddy: function (req, res) {
-  sails.log.info(res.send(req.boddy));
+  res.json(req.body);
+  //sails.log.info(res.send(req.boddy));
 },
 
 /* 5 Ejemplo Cookies*/
-metodoCokies: function (req, res){
+ metodoCokies: function (req, res){
   res.cookie('Nombre', 'Lilian', {signed:false} );
   res.cookie('Edad',24,{signed:false});
   res.send(req.cookies);
@@ -52,7 +54,9 @@ metodoCokies: function (req, res){
 
 /* 6 Ejemplo Fresh*/
 
-
+  metodoReqFresh: function (req,res) {
+    res.send(req.fresh);
+  },
 /*  7 Ejemplo Headers*/
 metodoReqheaders: function (req,res) {
   sails.log.info(res.send(req.headers));
@@ -66,7 +70,7 @@ metodoReqHost: function (req,res) {
 
 /* 9 Ejemplo Ip*/
 metodoReqIp : function (req,res) {
-  sails.log.info(res.send(req.IPv4));
+  res.send(req.ip);
 },
 
 /*10 Ejemplo Ips*/
@@ -98,11 +102,29 @@ metodoReqMethod: function (req, res) {
 
 /* 13 Ejemplo Options*/
 
+  metodoReqOptions : function (req,res) {
+    sails.log.info(res.send(req.options));
+  },
+
+  /* 13 Ejemplo Options*/
+ /* metodoOptions1: function filterByUser(req, res, next) {
+
+    if (req.session.me) {
+
+      req.options.where = {
+        userId: req.session.me
+      }
 
 
+
+    return next();
+
+  },
 /* 14 Params */
-metodoReqParams: function (req, res) {
-  sails.log.info(res.send(req.params));
+metodoReqParams: function (req, res , nombre) {
+  //sails.log.info(res.send(req.params));
+  res.send(req.params.nombre);
+
 },
 
 /* 15 Path*/
@@ -120,33 +142,88 @@ metodoReqProtocol: function (req,res) {
       return res.send("this is a secure HTTPS request");
       break;
   }
-}
-}
+},
+/* 17 Query*/
+//http://localhost:1337/Request/metodoReqQuery?q=tobi+ferret
+metodoReqQuery: function (req, res, q,order,shoe,color,type) {
+ sails.log.info(req.query);
+ //http://localhost:1337/Request/metodoReqQuery?q=tobi+ferret&order=desc&shoe[color]=blue&shoe[type]=converse
+ return res.send(req.q );
+ return res.send(req.order );
+  return res.send(req.shoe.color );
+  return res.send(req.shoe.type );
 
+},
+  /* 18 Secure*/
+  metodoReqSecure: function (req,res) {
+        return res.send(req.secure);
+    //http://localhost:1337/Request/metodoReqSecure
+    //imprime false porque solo es http no tiene seguridad esta ruta
+  },
+  /* 19 req.signedCookies*/
+  metodoReqSignedCookies: function (req,res) {
 
-=======
-module.exports = {
-    optionsValues: function (req, res, next) {
-        //req.options.values
-        //Los valores por defecto para los planos createy updateacciones.
-        sails.log.info(req.method);
-        if (req.session.user) {
-            // Use un existente req.options.values, o inicialicelo a un objeto vacio
-            req.options.values = req.options.values || {};
-            // Setear el default `name` para "crear" y "actualizar" planos
-            req.options.values.name = req.session.user.name;
-        }
-        return next();
-    },
-    optionsValuesWhere: function (req, res, next) {
-        // req.options.values.where
-        if (req.session.user) {
-            // Use un existente req.options.where, o inicialicelo a un objeto vacio
-            req.options.where = req.options.where || {};
-            // Setear el default `userId` para "buscar" y "actualizar" planos
-            req.options.where.userId = req.session.user.id;
-        }
-        return next();
+    res.cookie('Nombre', 'Lilian', {signed:false} );
+    res.cookie('Edad',24,{signed:true});
+    //res.send(req.cookies);
+    return res.send(req.signedCookies.Edad);
+  },
+  /* 20 req.socket*/
+  metodoReqSocket: function (req,res) {
+    if (req.isSocket) {
+      return res.send(' Low-level Socket.io methods and properties accessible on req.socket.');
     }
+    else {
+      return res.send(' This is not a request from a Socket.io client, so req.socket, may or may not exist.  If this is an HTTP request, req.socket is actually the underlying TCP socket.');
+    }
+
+  },
+
+  /* 21 req.subdomains*/
+  metodoReqsubdomains: function (req,res) {
+    return res.send(req.subdomains);
+  },
+
+  /* 22 req.subdomains*/
+
+  metodoReqUrl: function (req,res) {
+    return res.send(req.url);
+  },
+
+  /* 23 req.wantsJason*/
+
+  metodoReqwantsJson: function (req,res) {
+    
+      return res.json(req.wantsJSON);
+  },
+  
+  /* 24 req.subdomains*/
+  metodoReqxhr: function (req,res) {
+    return res.send(req.xhr);
+  }
 };
->>>>>>> origin/master
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DeberReqRes/api/models/Usuario.js
@@ -0,0 +1,28 @@
+/**
+ * Usuario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:{
+      type:"string"
+    },
+    password:{
+      type:"string"
+    },
+    apellidos:{
+      type:"string"
+    },
+    correo:{
+      type:"email"
+    },
+    fechaNacimiento:{
+      type:"date"
+    }
+  }
+};
+
